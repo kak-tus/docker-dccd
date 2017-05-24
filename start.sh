ed643cdd088729bdabb4a801942ef1da9937d422
Andrey Kuzmin <prepodam@mail.ru>
2017-05-24 21:28:37 +0300

    Docker permissions fix.
    When dockerfile creates files, they have some strange permissions.
    Recreate log dir to fix it.

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ groupdel www-data 2>/dev/null
 groupadd -g "$USER_GID" www-data
 useradd -d /home/www-data -g www-data -u "$USER_UID" www-data
 
+# docker permissions fix
+rm -rf /var/dcc/log
+mkdir -p /var/dcc/log
+
 chown -R www-data:www-data /var/dcc
 
 /var/dcc/libexec/rcDCC -m dccifd start &
